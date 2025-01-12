1074fb748c3ed25758ab2512a001ffbb9c89ad20
Sarah Zhao <110712068+sarahlz01@users.noreply.github.com>
2025-01-11 16:06:25 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-
-}
- 
-module.exports = nextConfig
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+export default nextConfig;
