37c58912a5c8dcc830dfe139d197a26fb67a6b28
ConnorG255 <25connorgriffin@gmail.com>
2025-01-11 21:48:52 -0800

    idk man something happened and now we are here

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,15 @@
 
 import OpenAI from "openai";
 import { useState } from "react";
-
+import {items} from "../components/item.tsx";
 
 
 
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_APTKEY,
   dangerouslyAllowBrowser: true });
+
+  //const Einput = document.getElementById('text');
 
 
 export default function Home() {
@@ -18,21 +20,35 @@ export default function Home() {
   const [response, setresponse] = useState("");
 
   const handleInputChange = (e) => {
+    /*if(e && e.keyCode == 13){
+      handleSubmit
+    } */
     setinput(e.target.value);
   };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const completion = await openai.chat.completions.create({
     model: "gpt-4o-mini", 
     store: true,
-    messages: [{ role: "user", content: "You are a kind and funny chatbot that specializes in providing items (with their respective links) for users to purchase. These are the items"
+    messages: [{ role: "user", content: "you are a chatbot that is going to give a list of random items to buy based off of a customers budget. Each item will be different."
+      + "The items will be random and preferably seen as useless or a waste of money. You will format the response as an array, listing of the item then link to that item."
+      + " Use the format in the parenthases as guidance: (ITEM1: LINK1, ITEM2: LINK2). Do not add anything else in your response other than the array of items."
+       +  "Do not add brackets and a quotation mark at the start and end of the response"
+       + "add quotation marks between each object."
+       + "Your customers budget is "
       + input
      }],
     });
-    setresponse(completion.choices[0]?.message?.content);
-
+   //setresponse(completion.choices[0]?.message?.content);
+    const finishedresponse = [completion.choices[0]?.message?.content];
+    for(var i = 0; i<finishedresponse.length; i++){
+      items(finishedresponse[i]);
+    }
   }
+  
+  
 
 
   return (
@@ -45,8 +61,9 @@ export default function Home() {
         <textarea
           value={input}
           onChange={handleInputChange}
-          placeholder="i donty know bruh"
+          placeholder="Budget"
           className="bg-slate-900 w-max"
+          type='text'
         />
         <br />
         <button type="submit" className=" p-2 bg-slate-700 focus: bg-slate-600">
---
+++ components/item.tsx
@@ -0,0 +1,8 @@
+
+export function items({item}) {
+    return (
+      <div class="flex flex-col gap-2 pt-2 w-full place-items-center 	">
+        {item}
+      </div>
+    );
+  }
